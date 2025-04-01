7e586b135d810e4cb6df3f7d917ea2a1b95cf93a
mouadispunk <bencaidmouad9@gmail.com>
2025-04-01 01:54:41 +0000

    created the data model for the monthly consumption submited by a Client

---
+++ models/consommation.php
@@ -0,0 +1,23 @@
+<?php
+class ConsommationMensuelle {
+    public $id;
+    public $kw;
+    private $imagePath;
+    private $createdAt;
+    
+    public function __construct($kw, $imagePath, $createdAt = null) {
+        $this->kw = $kw;
+        $this->imagePath = $imagePath;
+        $this->createdAt = $createdAt ?: new DateTime();
+    }
+    
+     public function getId() { return $this->id; }
+     public function getKw() { return $this->kw; }
+     public function getImagePath() { return $this->imagePath; }
+     public function getCreatedAt() { return $this->createdAt; }
+ 
+     public function setId($id) { $this->id = $id; }
+     public function setKw($kw) { $this->kw = $kw; }
+     public function setImagePath($imagePath) { $this->imagePath = $imagePath; }
+
+}
---
+++ traitement/consommationService.php
@@ -0,0 +1,4 @@
+<?php
+// as a client i need to submit a monthly submission (kw + image)
+// when i submit a new monthly consumption (it should be stored in the data base (monthly consumption)) 
+// i need a function that will take in the clientId and a   
